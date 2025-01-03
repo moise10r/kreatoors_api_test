350cf92a7ac618e031bf96f3f2d4deb6b9ec441c
moise10r <moiserushanika2006@gmail.com>
2025-01-03 08:53:10 +0200

    Set up mongodb and loggers.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express, { Router } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import Logger from "./lib/logger";
+import morganMiddleware from "./config/morganMiddleware";
 
 dotenv.config();
 
@@ -7,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(morganMiddleware);
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -16,4 +19,9 @@ router.get("/", (req, res) => {
 
 app.use("/api", router);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => {
+    app.listen(PORT, () => Logger.debug(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
